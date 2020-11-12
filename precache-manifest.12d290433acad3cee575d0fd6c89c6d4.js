867e58756536a91b6ab98bce34e93c5ae9ccd868
Fabricio Fraga <root@lnb024490bhz.ciandt.global>
2020-11-12 11:02:53 -0300

    first commit

---
+++ precache-manifest.12d290433acad3cee575d0fd6c89c6d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc0bc77f6a2505baad0bd5c6084792b",
+    "url": "/gestao-ride/index.html"
+  },
+  {
+    "revision": "38b8eb7ccb95388121ca",
+    "url": "/gestao-ride/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0d2e8659b77fe2bcf09d",
+    "url": "/gestao-ride/static/js/2.8b4f884c.chunk.js"
+  },
+  {
+    "revision": "38b8eb7ccb95388121ca",
+    "url": "/gestao-ride/static/js/main.d03dcc9a.chunk.js"
+  },
+  {
+    "revision": "8dd73849eb964ddbfc54",
+    "url": "/gestao-ride/static/js/runtime~main.67919a98.js"
+  }
+]);
